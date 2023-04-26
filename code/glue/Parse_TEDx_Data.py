a11b4e5c650a216b8b53f9b9fbf41ba6a99bd2e1
LoreSchaeffer <lm.lorenzo@outlook.it>
2023-04-26 11:28:46 +0200

    Watch Next

--- code/glue/Parse_TEDx_Data.py
+++ code/glue/Parse_TEDx_Data.py
@@ -67,7 +67,8 @@ watch_next_dataset_path = "s3://unibg-2023-data-tedx-lm/watch_next_dataset.csv"
 watch_next_dataset = spark.read.option("header", "true").csv(watch_next_dataset_path)
 
 watch_next_dataset = watch_next_dataset.drop("url")
-watch_next_dataset = watch_next_dataset.withColumnRenamed("idx", "idx_ref")
+watch_next_dataset = watch_next_dataset.groupBy(col("idx").alias("idx_ref")).agg(collect_list("watch_next_idx").alias("watch_next"))
+#watch_next_dataset = watch_next_dataset.withColumnRenamed("idx", "idx_ref")
 
 
 # AGGREGATE DATASETS
